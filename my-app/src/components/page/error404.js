9343e805ad42bb5422ed9e904cd360b5581f2353
Mantarraya <junior.tomairo@gmail.com>
2019-05-31 15:30:47 -0500

    Add 404 Not Found Page

--- my-app/src/components/page/error404.js
+++ my-app/src/components/page/error404.js
@@ -1,23 +1,42 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
 
-import  Footer from "../footer/footer";
+import Footer from "../footer/footer";
 import NavBar from "../navbar/navbar";
 
 import './error404.css'
 
-class Error404 extends Component{
+class Error404 extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <NavBar/>
+                <NavBar />
                 <div className="container error404--main">
-                    Escribe tu codigo para el 404
+
+                    <div id="notfound">
+                        <div class="notfound">
+                            <div class="notfound-404">
+
+                                <h1>Oops!</h1>
+                                <h2>404 - Página no encontrada</h2>
+                            </div>
+                            <br></br>
+
+                            <img src="https://media.giphy.com/media/Vg0RJZdxlAozNsspbu/giphy.gif" alt="Faztteach" width='500px' />
+
+                            <br></br>
+                            <a href="/">Regresar al inicio de página</a>
+                        </div>
+                    </div>
+
+
+
+
                 </div>
-                <Footer/>
+                <Footer />
             </div>
-            
+
         );
     }
 }
