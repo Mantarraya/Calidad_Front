4ab8906137507a45eb0501e6f68eac3f2589179d
Mantarraya <junior.tomairo@gmail.com>
2019-06-09 18:00:30 -0500

    Add initial structure HomeAdmin

---
+++ my-app/src/components/home_admin/home_admin.js
@@ -0,0 +1,17 @@
+import React,{ Component } from "react";
+
+
+class HomeAdmin extends Component{
+    
+    render(){
+
+        
+        return(
+            <div>
+                        foobar
+            </div>
+        );
+    }
+}
+
+export default withRouter(HomeAdmin);
