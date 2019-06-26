67130d491d6ea38ff0b8627b2c6ec33569dca3b7
Mantarraya <junior.tomairo@gmail.com>
2019-06-26 00:32:15 -0500

    import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 render(
     <BrowserRouter>
