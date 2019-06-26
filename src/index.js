35849b3a054ac21ec22b7f21ab2b8a34a5dd971f
Mantarraya <junior.tomairo@gmail.com>
2019-06-26 01:16:05 -0500

    route 3

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes";
-// using ES6 modules
 import { BrowserRouter, Route, Link } from "react-router-dom";
  
-// using CommonJS modules
-const BrowserRouter = require("react-router-dom").BrowserRouter;
-const Route = require("react-router-dom").Route;
-const Link = require("react-router-dom").Link;
 render(
     <BrowserRouter>
         <AppRoutes>
